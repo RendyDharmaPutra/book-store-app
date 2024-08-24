89cdf5d07e48ea58e0b463b68a136304e5bad7ff
archilst <rendydharmaputra424@gmail.com>
2024-08-24 21:45:18 +0700

    add visible and non-visible state on input type password

--- app/components/form/input.tsx
+++ app/components/form/input.tsx
@@ -1,21 +1,21 @@
-export default function Input({
-  name,
-  label,
-  error,
-  children,
-}: {
+type InputType = {
   name: string;
   label: string;
   error: string[] | null;
   children: React.ReactNode;
-}): JSX.Element {
+};
+
+export default function Input(props: InputType) {
   return (
-    <div key={name} className={`flex flex-col gap-1 w-full sm:w-[24rem]`}>
-      <label htmlFor={name} className="text-sm text-gray-600">
-        {label}
+    <div
+      key={props.name}
+      className={`relative flex flex-col gap-1 w-full sm:w-[24rem]`}
+    >
+      <label htmlFor={props.name} className="text-sm text-gray-600">
+        {props.label}
       </label>
-      {children}
-      {error && <p className="text-sm text-danger">{error}</p>}
+      {props.children}
+      {props.error && <p className="text-sm text-danger">{props.error}</p>}
     </div>
   );
 }
